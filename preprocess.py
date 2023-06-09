ad422ecc5c5c8492ddbf67a4f222a385503a284d
timothy <r10922188@csie.ntu.edu.tw>
2023-06-09 16:26:05 +0800

    add preprocess

---
+++ preprocess.py
@@ -0,0 +1,119 @@
+import argparse
+import os
+from glob import glob
+import pandas as pd
+from rich.progress import track
+from scipy.spatial.transform import Rotation as R
+
+
+parser = argparse.ArgumentParser(description='Preprocess data from VIU dataset')
+parser.add_argument('--dir', type=str, help='Path to the directory containing the data',required=True)
+parser.add_argument('--output_dir', type=str, help='Path to the directory to output the data',required=True)
+parser.add_argument('--type',type=str, help='Type of dataset, either VIU or Euroc',required=True)
+args = parser.parse_args()
+
+dir = args.dir
+output_dir = args.output_dir
+type = args.type
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+cnt = 0
+match type:
+    case 'VIU':
+        raise NotImplementedError
+    case 'Euroc':
+        dirs = glob(os.path.join(dir, '*'))
+        dirs.sort()
+        print(f'data dirs: {dirs}, mode {type}')
+        cnt = 0
+        for d in dirs:
+            os.mkdir(os.path.join(output_dir, f'{cnt:02d}'))
+            imu_data = pd.read_csv(os.path.join(d,'imu0/data.csv'))
+            print (f'imu data: {imu_data.columns}')
+            print (f'imu data:\n {imu_data.head()}')
+            imu_pt = 0
+            imu_data = imu_data.values
+            for i in range(imu_data.shape[0]):
+                imu_data[i][0] = int(imu_data[i][0])
+            #imu_data = imu_data[:,1:]
+            
+            gt_data = pd.read_csv(os.path.join(d,'state_groundtruth_estimate0/data.csv'))
+            print (f'gt data: {gt_data.columns}')
+            print (f'gt data:\n {gt_data.head()}')
+            gt_pt = 0
+            gt_data = gt_data.values
+            print (f'gt data: {gt_data[0][0]}')
+            for i in range(gt_data.shape[0]):
+                gt_data[i][0] = int(gt_data[i][0])
+            
+            #gt_data = gt_data[:,1:]
+            
+            photos = glob(os.path.join(d,'cam0/data/*.png'))
+            photos.sort()
+            times = []
+            last_photo = 0
+            last_imu = 0
+            last_gt = 0
+            for f in photos:
+                time = f.split('/')[-1].split('.')[0]
+                time = int(time)
+                times.append(time)
+                last_photo = time
+            
+            last_imu = imu_data[-1][0]
+            last_gt = gt_data[-1][0]
+            print (f'last photo: {last_photo}, last imu: {last_imu}, last gt: {last_gt}')
+            last = min(last_photo, last_imu, last_gt)
+    
+            begin = max(times[0],imu_data[0][0], gt_data[0][0])
+            
+            print (f'begin: {begin}, last: {last}')
+            
+            while imu_data[imu_pt][0] < begin:
+                imu_pt += 1
+            
+            #truncate photo
+            while times[0] < begin:
+                times.pop(0)
+                photos.pop(0)
+            pcnt = 0
+            while times[pcnt] <= last:
+                pcnt += 1
+                
+            #also remove the last one
+            times = times[:pcnt]
+            photos = photos[:pcnt-1]
+            gt_pose = []
+            for i in track(range(len(photos))):
+                f = photos[i]
+                os.system(f'cp {f} {output_dir}/{cnt:02d}/{f.split("/")[-1]}')
+                imu_at_time = []
+                gt_at_time = 0
+                while gt_data[gt_pt][0] < int(f.split('/')[-1].split('.')[0]):
+                    gt_pt += 1
+                gt_at_time = gt_data[gt_pt-1][1:]
+                gt_pose.append(gt_at_time)
+                # get imu data till the next photo
+                while imu_data[imu_pt][0] < times[i+1]:
+                    imu_at_time.append(imu_data[imu_pt][1:])
+                    imu_pt += 1
+                imu_f = open(os.path.join(output_dir,f'{cnt:02d}',f'{f.split("/")[-1].split(".")[0]}.txt'),'w')
+                for imu in imu_at_time:
+                    imu_f.write(f'{imu[0]},{imu[1]},{imu[2]},{imu[3]},{imu[4]},{imu[5]}\n')
+                imu_f.close()
+            gt_f = open(os.path.join(output_dir,f'{cnt:02d}','poses.txt'),'w')
+            for gt in gt_pose:
+                #parse to matrix
+                rx,ry,rz = gt[0],gt[1],gt[2]
+                tx,ty,tz = gt[3],gt[4],gt[5]
+                r = R.from_euler('xyz',[rx,ry,rz],degrees=True)
+                r = r.as_matrix()
+                gt_f.write(f'{r[0][0]},{r[0][1]},{r[0][2]},{tx},{r[1][0]},{r[1][1]},{r[1][2]},{ty},{r[2][0]},{r[2][1]},{r[2][2]},{tz}\n')
+            gt_f.close()
+            cnt += 1
+                
+    case _:
+        print (f'Dataset type {type} not supported')
+        raise NotImplementedError
+
--- test.py
+++ test.py
@@ -34,7 +34,7 @@ parser.add_argument('--rotation-mode', type=str, choices=['euler', 'quat'], defa
                     help='rotation mode for PoseExpnet : euler (yaw,pitch,roll) or quaternion (last 3 coefficients)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers')
-parser.add_argument('--epochs', default=15, type=int, metavar='N',
+parser.add_argument('--epochs', default=1, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--epoch-size', default=0, type=int, metavar='N',
                     help='manual epoch size (will match dataset size if not set)')
